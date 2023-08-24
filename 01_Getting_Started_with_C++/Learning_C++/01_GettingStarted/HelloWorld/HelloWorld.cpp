4c531ba41f3b661fdb365cbb6c3e2133200721d1
anthonyhenry <anthonyh1497@gmail.com>
2023-08-23 19:02:22 -0700

    Hello World Project

---
+++ 01_Getting_Started_with_C++/Learning_C++/01_GettingStarted/HelloWorld/HelloWorld.cpp
@@ -0,0 +1,17 @@
+//Hello World project
+
+//Include any libraries needed for the program
+#include <iostream> // iostream is a labrary that contains objects ans functions to print text to the screen and recieve texts from the keyboard
+
+//using namespace std;
+
+// Main function of the program
+int main()
+{
+	// Use the chracter output object from the iostream file of the standard library
+	// You can include the line 'using namespace std;' above to avoid needing to include 'std::' 
+	std::cout << "Hello World!" << std::endl;
+
+	// A return of 0 is is traditionally used to mean that no errors came up in the program
+	return(0);
+}
